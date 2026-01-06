571a9eedd42a4e917a98d7b19a837ea68397ba1b
Youssef TAKI <takiyoussefdev62@outlook.com>
2026-01-06 16:31:29 +0100

    adding timeframes functionality

---
+++ main.js
@@ -0,0 +1,81 @@
+// Selecting timeframe buttons and time display elements
+const dailyTimeframe = document.getElementById('daily');
+const weeklyTimeframe = document.getElementById('weekly');
+const monthlyTimeframe = document.getElementById('monthly');
+
+const titles = document.querySelectorAll('.title');
+const currentTime = document.querySelectorAll('.current-time');
+const previousTime = document.querySelectorAll('.previous-time');
+
+// fetch data from JSON file
+async function fetchData() {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+const data = fetchData();
+
+
+// Event listeners for timeframe buttons
+
+dailyTimeframe.addEventListener('click', event => {
+    // Update active timeframe button styling
+    event.target.classList.add('active-timeframe');
+    weeklyTimeframe.classList.remove('active-timeframe');
+    monthlyTimeframe.classList.remove('active-timeframe');
+
+    // Fetch data and update time displays
+    data.then(data => {
+        data.forEach(activity => {
+            titles.forEach((title, index) => {
+                if (title.textContent == activity.title) {
+                    currentTime[index].textContent = `${activity.timeframes.daily.current}hrs`;
+                    previousTime[index].textContent = `Last Day - ${activity.timeframes.daily.previous}hrs`;
+                }
+            });
+        });
+    });
+})
+
+weeklyTimeframe.addEventListener('click', event => {
+    // Update active timeframe button styling
+    event.target.classList.add('active-timeframe');
+    dailyTimeframe.classList.remove('active-timeframe');
+    monthlyTimeframe.classList.remove('active-timeframe');
+
+    // Fetch data and update time displays
+    data.then(data => {
+        data.forEach(activity => {
+            titles.forEach((title, index) => {
+                if (title.textContent == activity.title) {
+                    currentTime[index].textContent = `${activity.timeframes.weekly.current}hrs`;
+                    previousTime[index].textContent = `Last Week - ${activity.timeframes.weekly.previous}hrs`;
+                }
+            });
+        });
+    });
+})
+
+monthlyTimeframe.addEventListener('click', event => {
+    // Update active timeframe button styling
+    event.target.classList.add('active-timeframe');
+    dailyTimeframe.classList.remove('active-timeframe');
+    weeklyTimeframe.classList.remove('active-timeframe');
+
+    // Fetch data and update time displays
+    data.then(data => {
+        data.forEach(activity => {
+            titles.forEach((title, index) => {
+                if (title.textContent == activity.title) {
+                    currentTime[index].textContent = `${activity.timeframes.monthly.current}hrs`;
+                    previousTime[index].textContent = `Last Month - ${activity.timeframes.monthly.previous}hrs`;
+                }
+            });
+        });
+    });
+})
